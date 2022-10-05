88cfefe686202ce0fbc162f3d7088b128af77763
victoragara <115035596+victoragara@users.noreply.github.com>
2022-10-05 14:12:22 -0300

    SECTIONING

---
+++ storytime 2.py
@@ -0,0 +1,12 @@
+""""
+program: logic and programming string.py
+author: victor agara
+last date modefied: 2022/09/21
+story time part 2- assignment
+"""
+x= """" Hello my name is victor agara
+I am 20 years old that means i was born in 2001 and i have a black hair
+my favorite number is 9 but my lucky number is 99
+i have six siblings
+my favorite movie is ironman """
+print(x)
