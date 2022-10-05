d837783201c5d0f5d4141f68cabf27126bad8d97
victoragara <115035596+victoragara@users.noreply.github.com>
2022-10-05 14:33:20 -0300

    jay-v

---
+++ test2.py
@@ -0,0 +1,9 @@
+from os import nice
+my_age=int(20)
+my_weight=float(5.5)
+my_haircolour=str("chocolate")
+my_nickname=("chocolate")
+print(type(my_age))
+print(type(my_weight))
+print(type(my_nickname)
+print(type(my_nickname))
